1922a72401c594829541707fd35f7be157998ac5
Alex Murray <murray.alex@gmail.com>
2024-04-17 13:07:14 +0930

    static/setup-env.sh: handle case where keybase.env is not yet present
    Signed-off-by: Alex Murray <murray.alex@gmail.com>

--- static/setup-env.sh
+++ static/setup-env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-source "$SNAP_USER_DATA/.config/keybase/keybase.env"
-export $(grep -v '^#' "$SNAP_USER_DATA/.config/keybase/keybase.env" | xargs)
-
+if [ -f "$SNAP_USER_DATA/.config/keybase/keybase.env" ]; then
+  source "$SNAP_USER_DATA/.config/keybase/keybase.env"
+  export $(grep -v '^#' "$SNAP_USER_DATA/.config/keybase/keybase.env" | xargs)
+fi
 exec "$@" 
